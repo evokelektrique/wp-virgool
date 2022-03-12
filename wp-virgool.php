3bada9e15743fcfcbc04e8ad10f3d2868d6e28f5
EVOKE <evoke.lektrique@gmail.com>
2022-03-12 12:18:10 +0330

    First commit

---
+++ wp-virgool.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Virgool
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-virgool
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Virgool
+ */
+
+// Your code starts here.
